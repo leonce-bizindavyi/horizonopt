d79340bfc9fb25e7547c10d77aaf9f136b0cf5d6
leonce-bizindavyi <leonceb900@gmail.com>
2024-03-02 21:22:50 +0200

    first commit

---
+++ src/utils/siteMetaData.js
@@ -0,0 +1,22 @@
+// please update required information
+const siteMetadata = {
+    title: 'Next.js Blog With Tailwind CSS and Contentlayer',
+    author: 'CodeBucks',
+    headerTitle: 'Next.js Blog',
+    description: 'A blog created with Next.js, Tailwind.css and contentlayer.',
+    language: 'en-us',
+    theme: 'system', // system, dark or light
+    siteUrl: 'https://create-blog-with-nextjs.vercel.app', // your website URL
+    siteLogo: '/logo.png',
+    socialBanner: '/social-banner.png', // add social banner in the public folder
+    email: 'leonceb900@gmail.com', 
+    github: 'https://github.com/',
+    twitter: 'https://twitter.com/',
+    facebook: 'https://facebook.com',
+    youtube: 'https://www.youtube.com/@bizindavyileonce2224',
+    linkedin: 'https://www.linkedin.com//',
+    dribbble: 'https://www.dribbble.com',
+    locale: 'en-US',
+  }
+  
+  module.exports = siteMetadata
